76122cd4d3bdc11b422dff2cacca179828b5f320
Ben <teabens@outlook.com>
2015-07-07 12:25:49 -0700

    finished the bulk of both functions, need to test

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,29 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(original = matrix()) {
+  inverse <- NULL
+  
+  get <- function()
+  {
+    original
+  }
+  
+  set <- function(newMatrix)
+  {
+    original <<- newMatrix
+    inverse <<- NULL
+  }
+  
+  setInverse <- function(newInverse)
+  {
+    inverse <<- newInverse
+  }
+  
+  getInverse <- function()
+  {
+    inverse
+  }
 
 }
 
@@ -11,5 +33,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  
+  if(!is.null(inverse))
+  {
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  original <- x$get()
+  inverse <- solve(original)
+  x$setInverse(inverse)
+  inverse
 }
